0ab66a8d5e673904620c2b1f81a4a04f1a0bbdce
Haran <53114753+Ajiharan@users.noreply.github.com>
2019-07-21 22:29:05 +0530

    Rstdio_lap1
    R tutorial basic

---
+++ R_Language_lap1.R
@@ -0,0 +1,24 @@
+#creating variables
+a <- 2 
+print(a)
+
+x <-1:10
+x[x < 5]//display values that are lesser than 5
+
+//or statement
+x[x < 9 | x > 5]
+
+//and statement
+x[x < 9 & x > 5]
+
+
+y <-c(1,2,3,4)
+y//out put
+
+y1->y[y > 2]
+y1//out put
+
+y2 <-c(4,2,3,1,7,0)
+y3 <-y2[-3]//remove 3 from y2 and display
+
+
